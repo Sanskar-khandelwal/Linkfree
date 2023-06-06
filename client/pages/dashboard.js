2f117cdf7757a96f9f57fd4b8f77e1d62e2c368a
Sanskar khandelwal <Sanskarkhandelwal336@gmail.com>
2023-06-06 12:13:26 +0530

    added header and profile section in dashboard

--- client/pages/dashboard.js
+++ client/pages/dashboard.js
@@ -1,12 +1,39 @@
 import React, { useEffect } from "react"
 
+import LinkBox from "@/components/LinkBox"
+import UserHeader from "@/components/UserHeader"
+
 const dashboard = () => {
   useEffect(() => {
     if (!localStorage.getItem("LinkTreeToken"))
       return (window.location.href = "/login")
   }, [])
 
-  return <div>dashboard</div>
+  return (
+    <>
+      <div>
+        <UserHeader />
+        <main>
+          <section className="grid sm:grid-cols-2 xl:grid-cols-4 gap-4">
+            <LinkBox lbTitle="Links" lbNumber="5" lbSvg="url" lbTheme="white" />
+            <LinkBox
+              lbTitle="Growth"
+              lbNumber="5%"
+              lbSvg="growth"
+              lbTheme="blue"
+            />
+            <LinkBox
+              lbTitle="bubble"
+              lbNumber="5"
+              lbSvg="bubble"
+              lbTheme="white"
+            />
+            <LinkBox lbTitle="Look" lbNumber="5%" lbSvg="ig" lbTheme="blue" />
+          </section>
+        </main>
+      </div>
+    </>
+  )
 }
 
 export default dashboard
