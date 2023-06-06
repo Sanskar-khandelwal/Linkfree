2c33dc5ce5f2096c8f53a051a72306dfd0619fc1
Sanskar khandelwal <Sanskarkhandelwal336@gmail.com>
2023-06-07 00:09:18 +0530

    success fully made dynamic page

--- client/pages/dashboard.js
+++ client/pages/dashboard.js
@@ -1,21 +1,43 @@
-import React, { useEffect } from "react"
+import React, { useEffect,useState} from "react"
+import axios from 'axios'
+import {toast} from 'react-toastify'
 
 import LinkBox from "@/components/LinkBox"
 import UserHeader from "@/components/UserHeader"
 
+
 const dashboard = () => {
+  const [data, setData] = useState({})
   useEffect(() => {
     if (!localStorage.getItem("LinkTreeToken"))
       return (window.location.href = "/login")
+
+      axios.post("http://localhost:8080/data/dashboard", {
+        tokenMail: localStorage.getItem("LinkTreeToken")
+      },{
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then(res => {
+        const data = res.data
+        if(data.status == "error"){
+          return toast.error("Error Happened")
+        }
+        setData(data.userData)
+        localStorage.setItem('userHandle', data.userData.handle)
+        toast.success(data.message)
+      }).catch(err => {
+        console.log(err)
+      })
   }, [])
 
   return (
     <>
       <div>
-        <UserHeader />
+        <UserHeader data = {data} />
         <main>
           <section className="grid sm:grid-cols-2 xl:grid-cols-4 gap-4">
-            <LinkBox lbTitle="Links" lbNumber="5" lbSvg="url" lbTheme="white" />
+            <LinkBox lbTitle="Links" lbNumber={data.links} lbSvg="url" lbTheme="white" />
             <LinkBox
               lbTitle="Growth"
               lbNumber="5%"
