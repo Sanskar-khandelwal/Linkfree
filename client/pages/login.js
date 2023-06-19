2f846cb3c2c750c15c57ed8507dc4d5aed5a21c6
Sanskar Khandelwal <Sanskarkhandelwal336@gmail.com>
2023-06-20 03:19:09 +0530

    changed the design of login page

--- client/pages/login.js
+++ client/pages/login.js
@@ -9,6 +9,7 @@ const Login = () => {
   const router = useRouter()
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const [error, setError] = useState('')
   const handleLogin = (e) => {
     e.preventDefault()
 
@@ -25,16 +26,17 @@ const Login = () => {
     } ).then((response) => {
       const data = response.data
       console.log(data)
-      if (data.status === "success") {
+      if (data.status == "success") {
         toast("You are Login successfully")
         localStorage.setItem("LinkTreeToken", data.token)
         router.push('/dashboard')
       }
-      if(data.status == "not found"){
+      if(data.status == "error"){
         toast("User not found")
       }
     })
     .catch((error) => {
+      setError(error);
       console.log(error)
       toast.error(error.message)
     })
@@ -44,29 +46,28 @@ const Login = () => {
   return (
     <>
       <section
-        className={
-          styles.background + " min-h-screen flex justify-center items-center"
-        }
+       className="flex flex-row h-screen overflow-hidden"
       >
-        <div className="main">
-          <div className="content border-2 px-4 py-8 rounded-2xl shadow-lg bg-white">
-            <h1 className="text-2xl font-bold text-center">
-              Make your Visibility Easy
+        <div className="main flex  h-full px-10 w-2/5 items-center">
+          <div className="content px-4  bg-white w-full">
+            <h1 className="text-2xl font-bold text-left">
+              Enter Your Verse
             </h1>
-            <p className="text-center  mt-2">Make LinkTree, Make your Brand</p>
+            <p className="text-left  mt-2">Stay Connected, Stay Inspired</p>
             <form
               onSubmit={handleLogin}
-              className="flex flex-col gap-3 text-lg mt-3"
+              className="flex flex-col gap-3 text-lg mt-6 w-full"
             >
               <input
-                className="shadow-md  border-2 px-2 py-2  focus:outline-none"
+                className=" border rounded-md bg-gray-100 px-3 py-2  focus:outline-none w-full"
                 type="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 placeholder="Enter Your Mail"
               />
+              {error && <p> try different username </p>}
               <input
-                className="shadow-md  border-2 px-2 py-2  focus:outline-none"
+                className=" border rounded-md bg-gray-100 px-3 py-2  focus:outline-none w-full"
                 type="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
@@ -79,14 +80,18 @@ const Login = () => {
                 className="bg-indigo-600 text-white py-2 rounded-md cursor-pointer "
               />
             </form>
-          </div>
-          <h4 className="text-center text-white pt-3">
+            <h4 className="text-left text-black pt-3 ">
             New here ?{" "}
-            <Link className="font-bold text-indigo-500" href="/apply">
-              Apply here
+            <Link className="font-bold text-indigo-500 " href="/apply">
+              Apply Now
             </Link>
           </h4>
+          </div>    
         </div>
+        <div className="flex-1 w-3/5 h-full ">
+         <img src="/images/bulb-light.jpg " alt="" className="rounded-lg object-cover h-full w-full" />
+        </div>
+
       </section>
     </>
   )
