cff40b90eee1eafa8f92bfdc6ed30ed001dc8cb1
Sanskar Khandelwal <Sanskarkhandelwal336@gmail.com>
2023-06-22 01:20:53 +0530

    added a icon and maxwidth of user header is not 5xl

--- client/components/UserHeader.js
+++ client/components/UserHeader.js
@@ -46,7 +46,7 @@ const UserHeader = () => {
 
   return (
     <>
-      <header className="mt-3 flex flex-row justify-between items-center">
+      {/* <header className="mt-3 flex flex-row justify-between items-center">
         <div className="flex flex-col md:flex-row ">
           <Link href="/edit/links">
             <button className="inline-flex w-40 md:w-auto px-5 py-3 text-gray-600 font-bold border hover:text-gray-900 hover:bg-gray-100 rounded-md mb-3 mr-3">
@@ -86,7 +86,47 @@ const UserHeader = () => {
             />
           </div>
         </Link>
-      </header>
+      </header> */}
+      <header className="mt-3 flex flex-row justify-between items-center max-w-5xl mx-auto">
+  <div className="flex flex-col md:flex-row">
+    <Link href="/edit/links">
+      <button className="inline-flex w-40 md:w-auto px-5 py-3 text-gray-600 font-bold border hover:text-gray-900 hover:bg-gray-100 rounded-md mb-3 mr-3">
+        <img src="/svg/url.svg" alt="" className="w-4 h-4 mr-2" />
+        <span className="text-sm">Edit Link</span>
+      </button>
+    </Link>
+    <Link href="/edit/profile">
+      <button className="inline-flex w-40 md:w-auto px-5 py-3 text-gray-600 font-bold border hover:text-gray-900 hover:bg-gray-100 rounded mb-3">
+        <img src="/svg/profile.svg" alt="" className="w-4 h-4 mr-2" />
+        <span className="text-sm">Edit Profile</span>
+      </button>
+    </Link>
+  </div>
+
+  
+    <div className="flex flex-row items-center mb-4">
+    <Link href={`http://localhost:3000/${handle}`}> 
+      <div className="inline-flex items-center px-4 py-2 rounded-lg border border-gray-300 mr-4 hover:bg-gray-100">
+        <div className="flex flex-col ml-2">
+          <span className="text-xs text-gray-600 font-medium">{handle}</span>
+          <span className="text-xs text-gray-400">{role} Pack</span>
+        </div>
+        <div className="user-img">
+          <img src={avatar} alt="" className="w-10 h-10 ml-3 rounded-full" />
+        </div>
+      </div>
+      </Link>
+     
+      <img
+        className="w-5 h-5 cursor-pointer hover:opacity-75 ml-2"
+        src="/svg/logout.svg"
+        alt="Logout"
+        onClick={handleLogout}
+      />
+      </div>
+
+</header>
+
     </>
   )
 }
